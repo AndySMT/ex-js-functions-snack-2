7e03d995fe441bb60a5f028d10fca32f5212f361
Andy Simota <andyiulian.simota@gmail.com>
2025-03-12 11:19:20 +0100

    snack9 -forEach -sequenziale

---
+++ snack9.js
@@ -0,0 +1,22 @@
+//* 🎯 Snack 9 (Bonus)
+/* Creare una funzione che esegue una sequenza di operazioni con ritardi
+Scrivi una funzione sequenzaOperazioni che accetta un array di operazioni (funzioni) e un tempo di intervallo.
+
+Ogni operazione deve essere eseguita in sequenza con un ritardo uguale al tempo di intervallo. */
+
+function sequenzaOperazioni(arr, msDelay) {
+  arr.forEach((operazione, index) => {
+    setTimeout(() => {
+      operazione();
+    }, index * msDelay);
+  });
+}
+
+sequenzaOperazioni(
+  [
+    () => console.log("Operazione 1"),
+    () => console.log("Operazione 2"),
+    () => console.log("Operazione 3"),
+  ],
+  2000
+);
